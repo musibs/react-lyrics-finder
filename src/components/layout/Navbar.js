7d72d874a62a8626498faf5e28db682867010539
musibs <musib.somnath@gmail.com>
2019-12-25 17:51:05 +1100

    Added the Lyrics page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-dark mb-5">
-            <span className="navbar-brand mb-0 h1 mx-auto">LyricFinder</span>         
+            <span className="navbar-brand mb-0 h1 mx-auto">LyricsFinder</span>         
         </nav>
     )
 }
